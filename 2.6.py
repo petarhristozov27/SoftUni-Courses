f4e79969f1713113b396deee141e1be6d6bef592
petarhristozov27 <peter.hristozov27@gmail.com>
2026-05-28 16:01:43 +0300

    Delete 2.6.py

--- 2.6.py
+++
@@ -1,9 +0,0 @@
-num_string = int(input())
-
-for _ in range(num_string):
-    j = input()
-
-    if ("," in j) or ("_" in j) or ("." in j):
-        print(f"{j} is not pure!")
-    else:
-        print(f"{j} is pure.")
