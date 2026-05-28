9609d9c84bb695ab63860c817c7d63f95e3ec9c2
petarhristozov27 <peter.hristozov27@gmail.com>
2026-05-28 16:01:48 +0300

    Delete 2.7.py

--- 2.7.py
+++
@@ -1,9 +0,0 @@
-while True:
-    comm = str(input())
-    if comm == "End":
-        break
-    if comm != "SoftUni":
-        for i in comm:
-            print(i, end="")
-            print(i, end="")
-        print()
